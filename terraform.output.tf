0f67b347f2b62d6c3e2b43d371d83962a9f3f42f
Obay <ahmad.obay@gmail.com>
2021-06-09 14:44:03 +0300

    Adding Public IP to output

--- terraform.output.tf
+++ terraform.output.tf
@@ -1,3 +1,7 @@
 output "SSHCommand" {
   value = "sshpass -p '${var.AdminPassword}' ssh -o 'StrictHostKeyChecking no' ${var.AdminUsername}@${cloudflare_record.CFDNSRecord.hostname}"
 }
+
+output "PublicIP" {
+  value = "${cloudflare_record.CFDNSRecord.hostname}"
+}
