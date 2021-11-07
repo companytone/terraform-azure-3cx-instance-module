f9e4bf86664fec854698089931c29434c1ab026c
Obay <ahmad.obay@gmail.com>
2021-11-07 11:44:48 -0800

    Add storage account & file share

---
+++ resource.azurerm_azurerm_storage_account.AzureStorageAccount.tf
@@ -0,0 +1,11 @@
+resource "azurerm_storage_account" "AzureStorageAccount" {
+  name                     = lower("${var.CustomerName}storageaccount")
+  resource_group_name      = azurerm_resource_group.AzureResourceGroup.name
+  location                 = azurerm_resource_group.AzureResourceGroup.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Customer = "staging"
+  }
+}
---
+++ resource.azurerm_storage_share.AzureFileShare.tf
@@ -0,0 +1,6 @@
+resource "azurerm_storage_share" "example" {
+  name                 = "sharename"
+  storage_account_name = azurerm_storage_account.AzureStorageAccount.name
+  # tier                 = "Cool" # not implemented yet. Will need to be applied after the provisioning
+  quota                = 50
+}
