295f64547a0d06d231ee2b305cd93d43cec1cacb
Obay <ahmad.obay@gmail.com>
2021-11-07 11:44:05 -0800

    Add tags to resrouce groups

--- resource.azurerm_resource_group.AzureResourceGroup.tf
+++ resource.azurerm_resource_group.AzureResourceGroup.tf
@@ -1,5 +1,8 @@
 resource "azurerm_resource_group" "AzureResourceGroup" {
   name     = "CompanyToneCustomers-${var.CustomerName}"
   location = var.AzureResourceGroupLocation
+  tags = {
+    CostCenter = "${var.CustomerName}"
+  }
 }
 
