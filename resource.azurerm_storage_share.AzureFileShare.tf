1773d9d22e1998a50058030aa100b4546d8bcef0
Obay <ahmad.obay@gmail.com>
2022-01-17 08:46:34 -0800

    Formatting a tf file

--- resource.azurerm_storage_share.AzureFileShare.tf
+++ resource.azurerm_storage_share.AzureFileShare.tf
@@ -2,5 +2,5 @@ resource "azurerm_storage_share" "example" {
   name                 = "sharename"
   storage_account_name = azurerm_storage_account.AzureStorageAccount.name
   # tier                 = "Cool" # not implemented yet. Will need to be applied after the provisioning
-  quota                = 50
+  quota = 50
 }
