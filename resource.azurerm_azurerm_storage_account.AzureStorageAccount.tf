899fd4e94025b09ef51bf9dade173ddc1a253b68
Obay <ahmad.obay@gmail.com>
2022-02-23 11:53:48 -0800

    shorten storage account name

--- resource.azurerm_azurerm_storage_account.AzureStorageAccount.tf
+++ resource.azurerm_azurerm_storage_account.AzureStorageAccount.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "AzureStorageAccount" {
-  name                     = lower("${var.CustomerName}storageaccount")
+  name                     = lower("${var.CustomerName}sa")
   resource_group_name      = azurerm_resource_group.AzureResourceGroup.name
   location                 = azurerm_resource_group.AzureResourceGroup.location
   account_tier             = "Standard"
